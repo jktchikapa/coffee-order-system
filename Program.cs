8823772b9d0bbf57cb325d8aa9dafe69a844a71b
jktchikapa <jktchikapa@gmail.com>
2026-01-08 21:10:46 -0500

    Initial commit : set the project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CoffeeConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
